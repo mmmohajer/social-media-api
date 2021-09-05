624211db466c13a63adb9a761d9a6021517952f6
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2021-09-05 12:28:39 -0400

    DB and Middlewares added and app is initialized

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const morgan = require("morgan");
+require("dotenv").config();
+
+// app
+const app = express();
+
+// Database
+let dbConnectionString;
+if (process.env.NODE_ENV === "development") {
+  dbConnectionString = process.env.DATABASE_LOCAL;
+} else {
+  dbConnectionString = process.env.DATABASE_CLOUD;
+}
+mongoose
+  .connect(dbConnectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connected"));
+
+// Middlewares
+app.use(morgan("dev"));
+app.use(express.json({ limit: "50mb" }));
+app.use(helmet());
+app.use(cookieParser());
+if (process.env.NODE_ENV === "development") {
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+}
+
+// Port
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server is running on PORT ${port}`);
+});
